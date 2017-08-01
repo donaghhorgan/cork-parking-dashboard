5b1000acd16abad8de678ec39be6bd5e234bff40
Donagh Horgan <donagh.horgan@gmail.com>
2017-08-01 23:23:05 +0100

    Improve logging.

--- scrapers/carparks/scrape.py
+++ scrapers/carparks/scrape.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python
 import argparse
-import Geohash
 import json
+import logging
 import urllib
 
+import Geohash
 from influxdb import InfluxDBClient
 
-
-URL='http://data.corkcity.ie/api/action/datastore_search'
-RESOURCE_ID='6cc1028e-7388-4bc5-95b7-667a59aa76dc'
+URL = 'http://data.corkcity.ie/api/action/datastore_search'
+RESOURCE_ID = '6cc1028e-7388-4bc5-95b7-667a59aa76dc'
 
 
 def to_get_request(url, **kwargs):
@@ -36,26 +36,29 @@ def to_influxdb_point(record):
 
 
 if __name__ == '__main__':
-    # Parse command line arguments
-    parser = argparse.ArgumentParser()
-    parser.add_argument('username')
-    parser.add_argument('password')
-    parser.add_argument('--host', default='influxdb')
-    parser.add_argument('--port', type=int, default=8086)
-    parser.add_argument('--database', default='cork')
-    args = parser.parse_args()
+    logging.basicConfig(format='%(asctime)s %(message)s')
+    try:
+        # Parse command line arguments
+        parser = argparse.ArgumentParser()
+        parser.add_argument('username')
+        parser.add_argument('password')
+        parser.add_argument('--host', default='influxdb')
+        parser.add_argument('--port', type=int, default=8086)
+        parser.add_argument('--database', default='cork')
+        args = parser.parse_args()
 
-    # Request the latest data
-    url = to_get_request(URL, resource_id=RESOURCE_ID)
-    response = urllib.urlopen(url)
-    
-    # Convert the response data to InfluxDB point format
-    data = json.loads(response.read().decode('utf-8'))
-    points = list(map(to_influxdb_point, data['result']['records']))
-    
-    # Connect to InfluxDB and write the data
-    client = InfluxDBClient(args.host, args.port, args.username,
-                            args.password, args.database)
-    client.create_database(args.database)
-    client.write_points(points)
+        # Request the latest data
+        url = to_get_request(URL, resource_id=RESOURCE_ID)
+        response = urllib.urlopen(url)
 
+        # Convert the response data to InfluxDB point format
+        data = json.loads(response.read().decode('utf-8'))
+        points = list(map(to_influxdb_point, data['result']['records']))
+
+        # Connect to InfluxDB and write the data
+        client = InfluxDBClient(args.host, args.port, args.username,
+                                args.password, args.database)
+        client.create_database(args.database)
+        client.write_points(points)
+    except:
+        logging.exception('Could not scrape data')
